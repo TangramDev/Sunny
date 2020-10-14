82e86d924c4197cb63f5620901b12abd96aa394c
sunhui <sunhuizlz@yeah.net>
2020-10-14 23:03:44 +0800

    update to Sunny - version 1.0.0.3

--- src/SunnyCtrl/UserControl3.cs
+++ src/SunnyCtrl/UserControl3.cs
@@ -1,4 +1,17 @@
-﻿using System;
+﻿/********************************************************************************
+*					Sunny - version 1.0.0.3							*
+*********************************************************************************
+* Copyright (C) 2002-2020 by Tangram Team.   All Rights Reserved.				*
+*
+* This SOURCE CODE is governed by a BSD - style license that can be
+* found in the LICENSE file.
+*
+* CONTACT INFORMATION:
+* mailto:tangramteam@outlook.com
+* https://www.tangram.dev
+********************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
