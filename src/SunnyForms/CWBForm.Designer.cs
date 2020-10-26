bcfd0d14ec6ced16a8a0bb9794ae660c35b068a7
sunhui <sunhuizlz@yeah.net>
2020-10-27 00:09:54 +0800

    Fix bug for Form Sub-Application

--- src/SunnyForms/CWBForm.Designer.cs
+++ src/SunnyForms/CWBForm.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 68);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // CWBForm
             // 
@@ -36,6 +46,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Olive;
             this.ClientSize = new System.Drawing.Size(555, 923);
+            this.Controls.Add(this.button1);
             this.Name = "CWBForm";
             this.Text = "CWBForm";
             this.Load += new System.EventHandler(this.CWBForm_Load);
@@ -44,5 +55,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
